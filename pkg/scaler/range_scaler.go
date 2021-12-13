c957b76d93d29e9518f45f5c8bb7f63ef8be7487
eddy-kor-92 <16secondlife@gmail.com>
2021-12-13 14:34:02 +0900

    refactor range scaler & fix bugs

--- pkg/scaler/range_scaler.go
+++ pkg/scaler/range_scaler.go
@@ -1,13 +1,7 @@
 package scaler
 
 import (
-	"context"
-	"fmt"
-	"math/rand"
-
-	"k8s.io/api/autoscaling/v2beta2"
-	v1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"github.com/tmax-cloud/scheduled-scaler-operator/pkg/hpamanager"
 )
 
 // RangeScaler is ..
@@ -17,44 +11,14 @@ type RangeScaler struct {
 
 func (s *RangeScaler) Run() {
 	logger.Info("RangeScaler start running")
-	utilization := int32(50)
-	b := make([]byte, 6)
-	rand.Read(b)
-
-	hpaName := fmt.Sprintf("%s-%s-%x", s.scheduledScaler, s.target, b)
-	hpa := &v2beta2.HorizontalPodAutoscaler{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      hpaName,
-			Namespace: s.namespace,
-			Labels: map[string]string{
-				"owner": s.scheduledScaler,
-			},
-		},
-		Spec: v2beta2.HorizontalPodAutoscalerSpec{
-			ScaleTargetRef: v2beta2.CrossVersionObjectReference{
-				APIVersion: "apps/v1",
-				Kind:       "Deployment",
-				Name:       s.target,
-			},
-			MinReplicas: &s.schedule.MinReplicas,
-			MaxReplicas: s.schedule.MaxReplicas,
-			Metrics: []v2beta2.MetricSpec{
-				{
-					Type: v2beta2.ResourceMetricSourceType,
-					Resource: &v2beta2.ResourceMetricSource{
-						Name: v1.ResourceCPU,
-						Target: v2beta2.MetricTarget{
-							Type:               v2beta2.UtilizationMetricType,
-							AverageUtilization: &utilization,
-						},
-					},
-				},
-			},
-		},
-	}
-
-	if err := s.cl.Create(context.Background(), hpa); err != nil {
-		logger.Error(err, "Failed to create hpa in range scaler")
+	if err := hpamanager.UpdateHpa(s.cl, &hpamanager.HpaOptions{
+		Namespace:           s.namespace,
+		Target:              s.target,
+		ScheduledScalerName: s.scheduledScaler,
+		MinReplicas:         &s.schedule.MinReplicas,
+		MaxReplicas:         &s.schedule.MaxReplicas,
+	}); err != nil {
+		logger.Error(err, "Creating Hpa failed in Range scaler")
 		return
 	}
 
