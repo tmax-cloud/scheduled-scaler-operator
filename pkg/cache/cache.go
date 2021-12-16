4cfa12ac6d8399d233b2e459115fec2592792551
eddy-kor-92 <16secondlife@gmail.com>
2021-12-16 17:40:43 +0900

    Change to Interface each components

--- pkg/cache/cache.go
+++ pkg/cache/cache.go
@@ -7,17 +7,24 @@ import (
 	"github.com/tmax-cloud/scheduled-scaler-operator/pkg/apimanager"
 )
 
-type ScheduledScalerCache struct {
+type ScheduledScalerCache interface {
+	Put(*scscv1.ScheduledScaler) error
+	Get(*scscv1.ScheduledScaler) *scscv1.ScheduledScaler
+	Remove(*scscv1.ScheduledScaler)
+	HasChanged(*scscv1.ScheduledScaler) bool
+}
+
+type ScheduledScalerCacheImpl struct {
 	cached map[string]*scscv1.ScheduledScaler
 }
 
-func New() *ScheduledScalerCache {
-	return &ScheduledScalerCache{
+func New() ScheduledScalerCache {
+	return &ScheduledScalerCacheImpl{
 		cached: make(map[string]*scscv1.ScheduledScaler),
 	}
 }
 
-func (c *ScheduledScalerCache) Put(scsc *scscv1.ScheduledScaler) error {
+func (c *ScheduledScalerCacheImpl) Put(scsc *scscv1.ScheduledScaler) error {
 	if key := apimanager.GetNamespacedName(*scsc); key == "" {
 		return fmt.Errorf("There's empty namespaced name in scheduledScaler instance")
 	} else {
@@ -26,17 +33,17 @@ func (c *ScheduledScalerCache) Put(scsc *scscv1.ScheduledScaler) error {
 	}
 }
 
-func (c *ScheduledScalerCache) Get(scsc *scscv1.ScheduledScaler) *scscv1.ScheduledScaler {
+func (c *ScheduledScalerCacheImpl) Get(scsc *scscv1.ScheduledScaler) *scscv1.ScheduledScaler {
 	return c.cached[apimanager.GetNamespacedName(*scsc)]
 }
 
-func (c *ScheduledScalerCache) Remove(scsc *scscv1.ScheduledScaler) {
+func (c *ScheduledScalerCacheImpl) Remove(scsc *scscv1.ScheduledScaler) {
 	if c.exist(*scsc) {
 		delete(c.cached, apimanager.GetNamespacedName(*scsc))
 	}
 }
 
-func (c *ScheduledScalerCache) HasChanged(scsc *scscv1.ScheduledScaler) bool {
+func (c *ScheduledScalerCacheImpl) HasChanged(scsc *scscv1.ScheduledScaler) bool {
 	if !c.exist(*scsc) {
 		return true
 	}
@@ -45,7 +52,7 @@ func (c *ScheduledScalerCache) HasChanged(scsc *scscv1.ScheduledScaler) bool {
 	return scsc.ObjectMeta.Generation != oldScsc.ObjectMeta.Generation
 }
 
-func (c *ScheduledScalerCache) exist(scsc scscv1.ScheduledScaler) bool {
+func (c *ScheduledScalerCacheImpl) exist(scsc scscv1.ScheduledScaler) bool {
 	_, exist := c.cached[apimanager.GetNamespacedName(scsc)]
 	return exist
 }
